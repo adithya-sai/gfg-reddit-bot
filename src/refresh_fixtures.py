e61994a50385a911457ffca80103ff2a7941e75a
adithya-sai <adithyasai.n@gmail.com>
2019-10-02 20:50:02 -0700

    refresh only available league fixtures

--- src/refresh_fixtures.py
+++ src/refresh_fixtures.py
@@ -14,13 +14,14 @@ def refresh_fixtures():
     logger.info("Refreshing fixtures...")
     api_call_stat = list(Stat.query("api-call"))[0]
     api_stat_dict = api_call_stat.stat_value.as_dict()
+    available_leagues = list(Stat.query("available_leagues"))[0].stat_value.as_dict()["curr"]
     logger.info(api_stat_dict)
     logger.info("Current API call count : {}".format(api_stat_dict["count"]))
     if api_stat_dict["count"] < int(config.get("MaxApiCall")):
 
         fixtures_list = api.get_new_fixtures(config.get("TeamId"))
         for f in fixtures_list:
-            if f["statusShort"] == "NS" or f["statusShort"] == "TBD":
+            if (f["statusShort"] == "NS" or f["statusShort"] == "TBD") and f["league_id"] in available_leagues:
                 Fixture(fixture_id=f["fixture_id"], home=f["homeTeam"]["team_name"], away=f["awayTeam"]["team_name"],
                         home_team_id=f['homeTeam']['team_id'], away_team_id=f['awayTeam']['team_id'],
                         start_time=int(f["event_timestamp"]), status=f["statusShort"], result=None,
